9faf5acbaf14d8b00fa9c3302da2bd925db6d363
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-10 01:44:15 +0300

    Kotlin

---
+++ BasicClass/app/src/main/java/com/merttan/basicclass/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.merttan.basicclass
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.drawable.shapes.Shape
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.solver.widgets.Rectangle
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val d1 = LuckDispatcher()
+        val d2 = LuckDispatcher()
+
+        d1.getNumber()
+        d2.getNumber()
+        //-----------------Read-only list----------------
+        val list = listOf("a", "b", "c")
+        //-----------------Read-only map----------------
+        val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+
+        println(list)
+        println(map)
+        println(map["a"])
+
+        val myObject = object : MyAbstractClass() {
+            override fun doSomething() {
+// ...
+            }
+            override fun sleep() { // ...
+            }
+        }
+        myObject.doSomething()
+
+        val files = File("Test").listFiles()
+        println(files?.size)
+
+        //-----------------Swapping two variables----------------
+        var a = 1
+        var b = 20
+        a = b.also { b = a }
+        println(a)
+
+
+
+
+         }
+
+        abstract class MyAbstractClass {
+            abstract fun doSomething()
+            abstract fun sleep()
+        }
+
+        object Resource {
+            val name = "Name"
+        }
+        }
+
+        class LuckDispatcher {
+            fun getNumber() {
+                var objRandom = Random()
+                println(objRandom.nextInt(10))
+            }
+        }
---
+++ Collections/app/src/main/java/com/merttan/collections/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.merttan.collections
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val ListFruit = listOf("Ali", "Mehmet", "Fikret", "Amk")
+
+        for (item in ListFruit) {
+            println(ListFruit)
+        }
+        when {
+            "Mehmet" in ListFruit -> println("Mehmet listede kayıtlı ")
+
+        }
+        ListFruit
+                .filter { it.startsWith("A") }
+                .sortedBy { it }
+                .map { it.toUpperCase() }
+                .forEach { println(it) }
+
+    }
+}
