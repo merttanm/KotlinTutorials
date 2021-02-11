d238c4d2a10c7e52261ebe37dc4df6d042f472c9
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-11 23:22:23 +0300

    Kotlin

--- Idioms/app/src/main/java/com/merttan/idioms/MainActivity.kt
+++ Idioms/app/src/main/java/com/merttan/idioms/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.merttan.idioms
 
+import android.icu.util.RangeValueIterator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = listOf(1,2,4,5,6,7,8,9)
+        println(list)
+
+        for( i in 1..100){}
+        for( i in 1 until 100){}
+        for( x in 2..10 step 2){}
+        for( x in 10 downTo 1) {}
+
+
+        println(theAnswer())
+
+
+
+
+
+
     }
+    
+    fun theAnswer()=42
+
 }
