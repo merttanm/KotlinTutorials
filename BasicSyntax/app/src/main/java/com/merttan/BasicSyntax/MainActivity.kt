9ed29a2e8f8f5a5f5a3b4abc1742d1cb348af37c
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-13 18:29:41 +0300

    Chapter 1
    Var ve Val değişken tanımlamaları, bunlar ile aritmatik işlemler, ekrana basma , mutable enmutable konusu, fonksiyonlar

--- BasicSyntax/app/src/main/java/com/merttan/BasicSyntax/MainActivity.kt
+++ BasicSyntax/app/src/main/java/com/merttan/BasicSyntax/MainActivity.kt
@@ -25,25 +25,44 @@ class MainActivity : AppCompatActivity() {
         printmultiplication(5, 7)
 
 
-        println("----------------Veriables--------------")
+
+        println("----------------Değişken Türleri--------------")
         val a : Int=1
         val b = 2
         val c : Int
         c=3
         println(" a =$a, b=$b, c=$c")
 
+        // b=20 // Hata verir
+
         var x = 11
         x += 1
         println(" x=$x")
 
-        println("----------------Top-Level Veriables--------------")
+        println("----------------Mutable Objeler--------------")
+
+        /*üzerine ekleme silme değiştirme gibi işlemler yapılabilir */
+
+        val list =mutableListOf(1,2,3)
+        list.add(4)
+        println(list)// [1,2,3,4]
+
+        println("----------------Smart-Casted--------------")
+
+
+
+
+
+
+
+        println("----------------Top-Level Değişken Türleri--------------")
         println("vrbl=$vrbl; PI=$PI")
         increment()
         println("incrementX()")
         println("vrbl=$vrbl; PI=$PI")
 
 
-        println("----------------String Teamples--------------")
+        println("----------------Dize Örnekleri--------------")
 
         var d = 1
         val s1 = "a is $a"
@@ -51,51 +70,84 @@ class MainActivity : AppCompatActivity() {
         val s2 = "${s1.replace("is" , "vas")}, but now is $a"
         println(s2)
 
-        println("----------------Conditional expressions--------------")
+        println("----------------Koşullu ifadeler--------------")
 
         println("max number is ${maxOf(5,10)}")
 
-        println("----------------Nullable values--------------")
+        println("----------------Nullable Değerler--------------")
         printProduct("6", "7")
         printProduct("a", "7")
         printProduct("a", "b")
 
+        println("------------------------------")
+        println(fullName)
+        name="Numan"
+        println(fullName)
+        println("------------------------------")
+        println(fizzy) //42
+        println(buzz)  // CalculatE... 42
+
+        /*
+        Proje geliştirme aşamasında var atamak yerine val atanmalıdır.Sebebi ise sonradan
+        programı senkronize etmek veya mantık yürütmek gerektiği zaman sorunların ana kaynağı
+        olan mutasyon noktası sunmaz. Bu nedenle var yerine val tercih ediyoruz.
+        */
+            }
 
 
-    }
-    fun printSum(a: Int, b: Int): Unit {
-        println("sum of $a and $b is ${a + b}")
-    }
-    fun printmultiplication(a: Int, b: Int): Unit {
-        println("multiplication of $a and $b is ${a * b}")
-    }
-    fun increment(){
-        vrbl +=1
-    }
 
-    fun maxOf( number1 : Int, number2 : Int ) : Int{
-        if(number1>number2){
-            return number1
-        }else {
-            return number2
-        }
-    }
+            fun printSum(a: Int, b: Int): Unit {
+                println("sum of $a and $b is ${a + b}")
+            }
+            fun printmultiplication(a: Int, b: Int): Unit {
+                println("multiplication of $a and $b is ${a * b}")
+            }
+            fun increment(){
+                vrbl +=1
+            }
 
-    fun parseInt(str: String): Int? {
-        return str.toIntOrNull()
-    }
-    fun printProduct(arg1: String, arg2: String) {
-        val x = parseInt(arg1)
-        val y = parseInt(arg2)
-// Using `x * y` yields error because they may hold nulls.
-        if (x != null && y != null) {
-// x and y are automatically cast to non-nullable after null check
-            println(x * y)
-        }
-        else {
-            println("'$arg1' or '$arg2' is not a number")
-        }
-    }
+            fun maxOf( number1 : Int, number2 : Int ) : Int{
+                if(number1>number2){
+                    return number1
+                }else {
+                    return number2
+                }
+            }
+
+            fun parseInt(str: String): Int? {
+                return str.toIntOrNull()
+            }
+            fun printProduct(arg1: String, arg2: String) {
+                val x = parseInt(arg1)
+                val y = parseInt(arg2)
+                    // Using `x * y` yields error because they may hold nulls.
+                if (x != null && y != null) {
+                    // x and y are automatically cast to non-nullable after null check
+                    println(x * y)
+                }
+                else {
+                    println("'$arg1' or '$arg2' is not a number")
+                }
+            }
+            fun calculate():Int {
+                print("Calculate... ")
+                return 42
+            }
+            val fizzy=calculate()
+            val buzz
+            get()=calculate()
+
+
+            var name : String ="Mesut"
+            var surname: String="Ozil"
+            val fullName
+                get()="$name $surname"
+
+
+                 // ----------------Smart-Casted--------------
+
+
+
 
 
 }
