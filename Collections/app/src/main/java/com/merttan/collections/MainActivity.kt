8e3d242d4a3c79d80ddded546d5620c1f074347c
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-14 21:10:18 +0300

    Chapter 1

--- Collections/app/src/main/java/com/merttan/collections/MainActivity.kt
+++ Collections/app/src/main/java/com/merttan/collections/MainActivity.kt
@@ -7,6 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("-------------------Araylist-------------------")
+        val arrayList = arrayOf(1,2,3,4,5,6)
+        println(arrayList)
+
+        val myMsician= arrayListOf<String>("Tarkan","Tevetoğlu")
+        myMsician.add("Güllü")
+        myMsician.add(3,"Kadırgalı")
+        myMsician.add(4,"Aysel")
+        println(myMsician)
+
+        println("-------------------Map-------------------")
+
+
+
         val ListFruit = listOf("Ali", "Mehmet", "Fikret", "Amk")
 
         for (item in ListFruit) {
--- Idioms/app/src/main/java/com/merttan/idioms/MainActivity.kt
+++ Idioms/app/src/main/java/com/merttan/idioms/MainActivity.kt
@@ -12,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         val list = listOf(1,2,4,5,6,7,8,9)
         println(list)
 
+        val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+        println(map["a"])
+        println(map["b"])
+        println(map["c"])
+
+
+
+
         for( i in 1..100){}
         for( i in 1 until 100){}
         for( x in 2..10 step 2){}
