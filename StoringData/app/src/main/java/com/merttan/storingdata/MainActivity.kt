738bb3f2e69a79935c7dd27e9655652aaebef3c7
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-16 15:56:53 +0300

    Chapter 2
    sharedPreferences ile ilkel, basit verileri kolaylıkla kaydetme işlemleri

--- StoringData/app/src/main/java/com/merttan/storingdata/MainActivity.kt
+++ StoringData/app/src/main/java/com/merttan/storingdata/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.merttan.storingdata
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+     lateinit var sharedPreferences : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // SharedPreferences
+         sharedPreferences=this.getSharedPreferences("com.merttan.storingdata", MODE_PRIVATE)
+         val getSharedPreferences= sharedPreferences.getInt("age",-1)
+        if(getSharedPreferences==-1){
+            println("Hata")
+        } else{
+            //Emülator yeniden açılınca son kaydedilen anahtar kelimeyi Text e basar
+           textView.text="Your age: $getSharedPreferences"
+        }
     }
 
     fun save(view : View){
 
-        // SharedPreferences
+
         var text= editText.text.toString().toIntOrNull()
 
         if(text!=null) {
             textView.text = "Age :$text"
+            println("--------Veri Kaydedildi--------")
+            sharedPreferences.edit().putInt("age", text).apply() // Veriyi kaydeder
+
         }
     }
     fun delete(view :View){
