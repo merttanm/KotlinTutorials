e3f59066cc7972e7ad4d13cebc60784d8d8e3a71
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-20 22:28:05 +0300

    Chapter 2

--- ArtBookApp/app/src/main/java/com/merttan/artbookapp/MainActivity.kt
+++ ArtBookApp/app/src/main/java/com/merttan/artbookapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.merttan.artbookapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // biraz once oluşturduğum menüyü buraya bağlıyoruz
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.add_art,menu)
+        return  super.onCreateOptionsMenu(menu)
+
+    }
+
+    // seçilen itemin ne olduğunu anlayıp ona göre iş yapturuyoruz
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId== R.id.add_art_item){
+            val intent= Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
---
+++ ArtBookApp/app/src/main/java/com/merttan/artbookapp/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.merttan.artbookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+
+    fun selectImage(view : View){
+
+    }
+}
