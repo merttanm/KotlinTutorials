f9be33b61bf1d5c7f45aabeb08395be74b3ebedb
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-20 18:12:32 +0300

    Chapter 2
    SQLite ile veri kaydı , güncelleme, silme , arama gibi  temel veri tabanı fonksiyonları ile işlemler.

--- SQLiteKotlin/app/src/main/java/com/merttan/sqlitekotlin/MainActivity.kt
+++ SQLiteKotlin/app/src/main/java/com/merttan/sqlitekotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.merttan.sqlitekotlin
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,26 +8,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         try {
-            val myDatabase =this.openOrCreateDatabase("Muzisyenler", MODE_PRIVATE, null)  // Veri tabanı oluşturuyoruz
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS Muzisyenler (isim VARCHAR, yas INT)")    // Tablo oluşturuyor
-            myDatabase.execSQL("INSERT INTO Muzisyenler (isim, yas) VALUES ('mert',25)")            // Tabloya veri ekliyor
 
-            var cursor = myDatabase.rawQuery("SELECT * FROM Muzisyenler", null)        // Tablo içerisindeki her veriyi çekiyoruz
-            var nameIndex = cursor.getColumnIndex("isim")
-            var ageIndex = cursor.getColumnIndex("yas")
+            val myDatabase = this.openOrCreateDatabase("Musicians", Context.MODE_PRIVATE,null)   // Veri tabanı oluşturuyoruz
 
-            while(cursor.moveToNext()){
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (id INTEGER PRIMARY KEY,name VARCHAR, age INT)") // Tablo oluşturuyor
+
+           // myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('Mert',25)") // Tabloya veri ekliyor
+            //myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('Veli',23)") // Tabloya veri ekliyor
+           // myDatabase.execSQL("INSERT INTO musicians (name, age) VALUES ('Van persie',31)") // Tabloya veri ekliyor
+
+            //myDatabase.execSQL("UPDATE musicians SET age = 61 WHERE name = 'Veli'")  // veliye ait kaydın yaşını değiştiriyot
+            //myDatabase.execSQL("UPDATE musicians SET name = 'Kirk Hammett' WHERE id = 3") // ıd=3 olan kaydın adını değiştiriyor
+
+            myDatabase.execSQL("DELETE FROM musicians WHERE name = 'Veli'") // veliye ait kaydı siliyor
+
+
+            //val cursor = myDatabase.rawQuery("SELECT * FROM musicians WHERE id = 3",null)
+
+            //val cursor = myDatabase.rawQuery("SELECT * FROM musicians WHERE name LIKE 'K%'",null)
+
+            val cursor = myDatabase.rawQuery("SELECT * FROM musicians",null)  // Tablo içerisindeki her veriyi çekiyoruz
+
+            val nameIx = cursor.getColumnIndex("name")
+            val ageIx = cursor.getColumnIndex("age")
+            val idIx = cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()) {
                 // indexleri gezecek
-                println("Name: " +cursor.getString(nameIndex))
-
-                println("Age: " +cursor.getString(ageIndex))
-
+                println("Name: " + cursor.getString(nameIx))
+                println("Age: " + cursor.getInt(ageIx))
+                println("Id: " + cursor.getInt(idIx))
             }
+
             cursor.close()
 
-        }catch (e :Exception){
+
+        } catch (e: Exception) {
             e.printStackTrace()
         }
 
