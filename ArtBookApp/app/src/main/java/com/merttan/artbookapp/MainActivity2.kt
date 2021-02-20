558c33dd34b4646d228953199a3c01c68b6720c1
merttanm <68250572+merttanm@users.noreply.github.com>
2021-02-20 23:58:19 +0300

    Chapter 2

--- ArtBookApp/app/src/main/java/com/merttan/artbookapp/MainActivity2.kt
+++ ArtBookApp/app/src/main/java/com/merttan/artbookapp/MainActivity2.kt
@@ -1,8 +1,11 @@
 package com.merttan.artbookapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,14 @@ class MainActivity2 : AppCompatActivity() {
 
 
     fun selectImage(view : View){
+/*
+Kullanıcıdan uygulama çalışırken galerisini kullanmak için izin isteriz
+bu işlemi AndroidMnifest dosyasında yaparız. Eğer ki kullanıcıdan izin
+aldıysak ve kullanıcı daha önceden bu izni onayladıysa bunu her seferinde
+istemeyiz. Android 23 sürümü öncesi bu izin işlemleri gerekmiyordu*/
+
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+
 
     }
 }
