8c6c692c5008a2e50edb27c22c49de51fe2c2b95
merttanm <68250572+merttanm@users.noreply.github.com>
2021-05-05 23:10:24 +0300

    Bilyoner

--- BetApp/app/src/main/java/com/merttan/betapp/Login.java
+++ BetApp/app/src/main/java/com/merttan/betapp/Login.java
@@ -2,13 +2,32 @@ package com.merttan.betapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button signup;
+
+    public void init(){
+        signup=(Button)findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy= new Intent(Login.this,AnaSayfa.class);
+                startActivity(toy);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+
+
     }
 }
